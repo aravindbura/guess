0e18f854af5920b34f2b4ad33e95977dc35c79c7
aravindbura <78644762+aravindbura@users.noreply.github.com>
2021-07-21 21:27:11 +0530

    Update king.py

--- know/king.py
+++ know/king.py
@@ -2,9 +2,6 @@ import random
 
 num = random.randint(1,100)
 
-
-
-
 print("WELCOME TO GUESS ME!")
 print("I'm thinking of a number between 1 and 100")
 print("If your guess is more than 10 away from my number, I'll tell you you're COLD")
